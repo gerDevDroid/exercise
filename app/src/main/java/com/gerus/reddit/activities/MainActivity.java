aa41575d621215be7e210e06cc9c707e2c008aa9
Gerus-Mac <gerardo.guzmand@gmail.com>
2017-03-14 19:19:23 -0600

    Create network off
    - Add icon colors.
    - New user permission (ACCESS_NETWORK_STATE)
    - New UNetwork file.

--- app/src/main/java/com/gerus/reddit/activities/MainActivity.java
+++ app/src/main/java/com/gerus/reddit/activities/MainActivity.java
@@ -1,15 +1,20 @@
 package com.gerus.reddit.activities;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.gerus.reddit.R;
+import com.gerus.reddit.utils.UNetwork;
 
 public class MainActivity extends AppCompatActivity {
+    private Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mContext = this;
     }
 }
