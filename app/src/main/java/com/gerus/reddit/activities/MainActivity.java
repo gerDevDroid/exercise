a24ef1bf2aed75a15fb9bf14c902cb866463faf1
Gerus-Mac <gerardo.guzmand@gmail.com>
2017-03-14 18:43:18 -0600

    Create folder structure

--- app/src/main/java/com/gerus/reddit/activities/MainActivity.java
+++ app/src/main/java/com/gerus/reddit/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.gerus.reddit;
+package com.gerus.reddit.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.gerus.reddit.R;
 
 public class MainActivity extends AppCompatActivity {
 
